74c3c6e6829c11c444d5ed270a891bcd98cdd223
Oleksandr Sabov <alexsabov@gmail.com>
2015-01-21 12:08:33 +0100

    Add data

--- source/server/models/application.js
+++ source/server/models/application.js
@@ -14,4 +14,63 @@ var ApplicationSchema = new Schema({
 
 Application = mongoose.model('applications', ApplicationSchema);
 
+Application.remove({}, function(err) {
+    console.log('collection removed');
+});
+
+Application.create({
+    "name": "notepad++",
+    "platform": "windows",
+    "shortcuts": [
+        {
+            "combination": "ctrl+a",
+            "action": "Select all"
+        },
+        {
+            "combination": "ctrl+v",
+            "action": "Paste text"
+        },
+        {
+            "combination": "ctrl+c",
+            "action": "Copy text"
+        },
+        {
+            "combination": "ctrl+w",
+            "action": "Close current document"
+        },
+        {
+            "combination": "ctrl+a",
+            "action": "Select all"
+        },
+        {
+            "combination": "ctrl+v",
+            "action": "Paste text"
+        },
+        {
+            "combination": "ctrl+c",
+            "action": "Copy text"
+        },
+        {
+            "combination": "ctrl+w",
+            "action": "Close current document"
+        },
+        {
+            "combination": "ctrl+a",
+            "action": "Select all"
+        },
+        {
+            "combination": "ctrl+v",
+            "action": "Paste text"
+        },
+        {
+            "combination": "ctrl+c",
+            "action": "Copy text"
+        },
+        {
+            "combination": "ctrl+w",
+            "action": "Close current document"
+        }
+    ]
+});
+
 module.exports = Application;
