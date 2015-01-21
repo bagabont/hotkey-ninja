80e25125a84a2ac9f1a4b7dde4be5cb10fc1d6b7
Oleksandr Sabov <alexsabov@gmail.com>
2015-01-21 11:05:18 +0100

    Fix fighting

--- source/public/js/fighters.js
+++ source/public/js/fighters.js
@@ -16,7 +16,7 @@
                 fighters: fighters,
                 callbacks: {
                     attack: function (f, o, l) {
-                        if (o.getName() === 'kano') {
+                        if (o.getName() === fighters[1].name) {
                             self.setLife($('.player_1'), o.getLife());
                         } else {
                             self.setLife($('.player_2'), o.getLife());
@@ -34,27 +34,36 @@
             container.find(".player__health-inner").width(life + "%");
         },
 
-        kick: function () {
+        walkToCenter: function() {
+            console.log("once");
             mk.game._moveFighter(mk.game.fighters[0], "walking");
+            mk.game._moveFighter(mk.game.fighters[1], "walking-backward");
             setTimeout(function() {
                 mk.game._moveFighter(mk.game.fighters[0], "stand");
-                mk.game._moveFighter(mk.game.fighters[0], kick.toLowerCase().replace("_", "-"));
-            }, 1000);
+                mk.game._moveFighter(mk.game.fighters[1], "stand");
+            }, 800);
+        },
+
+        first: true,
+
+        kick: function () {
+            if (this.first) {
+                this.walkToCenter();
+                this.first = false;
+            } else {
+                mk.game._moveFighter(mk.game.fighters[0], _.sample(this.moves).toLowerCase().replace(/_/g, "-"));
+            }
         },
         opponentKick: function() {
-            kick = "FORWARD-JUMP-KICK";
-            mk.game._moveFighter(mk.game.fighters[1], kick.toLowerCase().replace("_", "-"));
+            mk.game._moveFighter(mk.game.fighters[1], _.sample(this.moves).toLowerCase().replace(/_/g, "-"));
         },
 
         moves: [
             "HIGH_KICK",
             "LOW_KICK",
-            "LOW_PUNCH",
             "HIGH_PUNCH",
-            "UPPERCUT",
             "SQUAT_LOW_KICK",
             "SQUAT_HIGH_KICK",
-            "SQUAT_LOW_PUNCH",
         ]
     };
 })();
