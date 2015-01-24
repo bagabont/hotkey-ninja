b5786c309523848755bcd3fa1aff044b05566026
Oleksandr Sabov <alexsabov@gmail.com>
2015-01-24 15:59:15 +0100

    Fix question layout

--- source/server/models/application.js
+++ source/server/models/application.js
@@ -39,6 +39,78 @@ Application.create({
             "action": "Close current document"
         },
         {
+            "combination": "ctrl+z",
+            "action": "Undo"
+        },
+        {
+            "combination": "ctrl+y",
+            "action": "Redo"
+        },
+        {
+            "combination": "ctrl+f",
+            "action": "Launch Find Dialog"
+        },
+        {
+            "combination": "ctrl+h",
+            "action": "Launch Find / Replace Dialog"
+        },
+        {
+            "combination": "ctrl+d",
+            "action": "Duplicate Current Line"
+        },
+        {
+            "combination": "ctrl+l",
+            "action": "Delete Current Line"
+        },
+        {
+            "combination": "ctrl+shift+f",
+            "action": "Find in Files"
+        },
+        {
+            "combination": "ctrl+shift-i",
+            "action": "Incremental Search"
+        },
+        {
+            "combination": "ctrl+s",
+            "action": "Save File"
+        },
+        {
+            "combination": "ctrl+alt+s",
+            "action": "Save As"
+        },
+        {
+            "combination": "ctrl+shift-s",
+            "action": "Save All"
+        },
+        {
+            "combination": "ctrl+o",
+            "action": "Open File"
+        },
+        {
+            "combination": "ctrl+n",
+            "action": "New file"
+        },
+        {
+            "combination": "ctrl+g",
+            "action": "Launch GoToLine Dialog"
+        },
+        {
+            "combination": "ctrl+shift+up",
+            "action": "Move Current Line Up"
+        },
+        {
+            "combination": "ctrl+alt+f",
+            "action": "Collapse the Current Level"
+        },
+        {
+            "combination": "ctrl+shift+down",
+            "action": "Move Current Line Down"
+        },
+        {
+            "combination": "ctrl+del",
+            "action": "Delete to end of word"
+        },
+        {
             "combination": "ctrl+a",
             "action": "Select all"
         },
@@ -55,20 +127,12 @@ Application.create({
             "action": "Close current document"
         },
         {
-            "combination": "ctrl+a",
-            "action": "Select all"
+            "combination": "ctrl+z",
+            "action": "Undo"
         },
         {
-            "combination": "ctrl+v",
-            "action": "Paste text"
-        },
-        {
-            "combination": "ctrl+c",
-            "action": "Copy text"
-        },
-        {
-            "combination": "ctrl+w",
-            "action": "Close current document"
+            "combination": "ctrl+y",
+            "action": "Redo"
         }
     ]
 });
