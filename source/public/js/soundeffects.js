0a5c2931ea8cb6658cbc33a9aac5956d91cad770
Momchil Marinov <mmarinov@spbtv.com>
2015-02-01 22:16:43 +0100

    fixed sound volume

--- source/public/js/soundeffects.js
+++ source/public/js/soundeffects.js
@@ -1,7 +1,6 @@
 var Sounds = {
     init: function () {
         var self = this;
-        var initVolume = 0.5;
         self.kickSounds = [
             "/sounds/Jab.mp3",
             "/sounds/Kick.mp3",
@@ -11,48 +10,43 @@ var Sounds = {
             "/sounds/UpperCut.mp3"
         ];
         self.backgroundSound = null;
-        self.volume = initVolume;
+        self.isPlaying = true;
+
         $(".volume").on("click", function () {
-            if (self.volume != 0) {
-                self.volume = 0;
+            if (self.isPlaying) {
                 self.pauseBackgroundSound();
                 $(".volume").removeClass("glyphicon-volume-up").addClass("glyphicon-volume-off");
             } else {
-                self.volume = initVolume;
                 self.playBackgroundSound();
                 $(".volume").removeClass("glyphicon-volume-off").addClass("glyphicon-volume-up");
             }
+            self.isPlaying = !self.isPlaying;
         });
     },
 
     playBackgroundSound: function () {
         var self = this;
         self.pauseBackgroundSound();
-        self.backgroundSound = self.play('/sounds/backgroundsound2.mp3', true);
-        self.backgroundSound.volume(0.01);
+        self.backgroundSound = self.play('/sounds/backgroundsound2.mp3', true, 0.01);
     },
     pauseBackgroundSound: function () {
         var self = this;
         if (self.backgroundSound != null) {
-            console.log(self.backgroundSound);
             self.backgroundSound.stop();
             self.backgroundSound.mute();
         }
     },
     playKickSound: function () {
         var self = this;
-        self.play(_.sample(self.kickSounds), false);
-
+        self.play(_.sample(self.kickSounds), false, 1.0);
     },
-    play: function (url, param) {
-        self = this;
-        var sound = new Howl({
+    play: function (url, loop, volume) {
+        return new Howl({
             urls: [url],
-            volume: self.volume,
+            volume: volume,
             autoplay: true,
-            loop: param
+            loop: loop
         }).play();
-        return sound;
     }
 };
 
